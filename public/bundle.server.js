15a2ee3288500049dc5189b5ebd541ec56896fd4
Mavrick91 <b_boy_du_91@hotmail.fr>
2017-09-21 21:56:01 +0200

    update bundle heroku

--- public/bundle.server.js
+++ public/bundle.server.js
@@ -9914,7 +9914,7 @@ var _default = {
 	title: 'RECETA ARGENTINA',
 	imagePipilet: _pipiletCooker2.default,
 	recipe: [{
-		title: 'MANGO FOOL',
+		title: 'Alfajores argentinos',
 		image: _argentine2.default,
 
 		sectionIngredient: [{
@@ -14487,7 +14487,11 @@ var _default = {
 	'download': _song_esp_twoTigers2.default,
 	'title': 'Dos tigres viejos',
 	'language': '(Español)',
-	'couplet': [['Dos tigres viejos', 'Dos tigres viejos', 'Corriendo muy rápido', 'Corriendo muy rápido', 'Uno no tiene ojos', 'Uno no tiene rabo', '¡Qué raro!', '¡Qué raro']]
+	'couplet': [['Dos tigres viejos', 'Dos tigres viejos', 'Corriendo muy rápido', 'Corriendo muy rápido', 'Uno no tiene ojos', 'Uno no tiene rabo', '¡Qué raro!', '¡Qué raro']],
+	information: {
+		title: 'Pinyin',
+		description: ['liang3 zhi3 lao3 hu3', 'liang3 zhi3 lao3 hu3', 'pao3 de kuai4', 'pao3 de kuai4', 'yi4 zhi3 mei2 you3 yan3 jing1', 'yi4 zhi3 mei2 you3 wei3 ba', 'zhen1qi2 guai4', 'zhen1 qi2 guai4']
+	}
 };
 exports.default = _default;
 ;
